35fc4623e58fc73fd3a579d15147f58a23a67718
D Schaedler <dominicschaedler@gmail.com>
2021-07-16 19:09:33 -0400

    Add Checkboxes fix require add checkbox add checkbox_value add remove_checkbox add ui_udpate

--- app/main.rb
+++ app/main.rb
@@ -1,9 +1,18 @@
 # frozen_string_literal: true
 
-require 'lib/standalone_ui.rb'
+require 'lib/standalone_ui'
 
 def tick(args)
   include SUI
-  new_bar = SUI.bar(x: 100, y: 100, percent: 0.75)
+  percent_ticker = args.state.tick_count.mod(100) / 100
+  new_bar = SUI.bar(x: 20, y: 680, percent: percent_ticker)
   args.outputs.primitives << new_bar
+
+  new_checkbox = SUI.checkbox(id: :first, x: 20, y: 640)
+  args.outputs.primitives << new_checkbox
+
+  checkbox_value = SUI.checkbox_value(id: :first)
+  args.outputs.labels << [60, 660, checkbox_value.to_s]
+
+  SUI.ui_update
 end
--- lib/standalone_ui.rb
+++ lib/standalone_ui.rb
@@ -9,10 +9,42 @@ module SUI
     bar_color = hex_to_rgb(bar_color)
     sui_primitive = []
     if border
-      sui_primitive << { x: x, y: y, w: w, h: h, r: border_color[0], g: border_color[1], b: border_color[2], primitive_marker: :border }
-      sui_primitive << { x: x + padding, y: y + padding, w: w * percent - padding * 2, h: h - padding * 2, r: bar_color[0],g: bar_color[1], b: bar_color[2], primitive_marker: :solid }
+      sui_primitive << { x: x, y: y, w: w, h: h, r: border_color[0], g: border_color[1], b: border_color[2],
+                         primitive_marker: :border }
+      sui_primitive << { x: x + padding, y: y + padding, w: w * percent - padding * 2, h: h - padding * 2,
+                         r: bar_color[0], g: bar_color[1], b: bar_color[2], primitive_marker: :solid }
     else
-      sui_primitive << { x: x, y: y, w: w * percent, h: h, r: bar_color[0], g: bar_color[1], b: bar_color[2],primitive_marker: :solid }
+      sui_primitive << { x: x, y: y, w: w * percent, h: h, r: bar_color[0], g: bar_color[1], b: bar_color[2],
+                         primitive_marker: :solid }
+    end
+  end
+
+  def checkbox(id:, x:, y:, w: 20, h: 20, box_color: '#000000', fill_color: '#000000', padding: 2, checked: false)
+    $gtk.args.state.checkboxes ||= {}
+    $gtk.args.state.checkboxes[id] ||= { id: id, x: x, y: y, w: w, h: h, box_color: hex_to_rgb(box_color),
+                                         fill_color: hex_to_rgb(fill_color), padding: padding, checked: checked }
+    sui_primitive = []
+    if $gtk.args.state.checkboxes[id][:checked]
+      sui_primitive << { x: x + padding, y: y + padding, w: w - padding * 2, h: h - padding * 2, r: fill_color[0],
+                         g: fill_color[1], b: fill_color[2], primitive_marker: :solid }
+    end
+    sui_primitive << { x: x, y: y, w: w, h: h, r: box_color[0], g: box_color[1], b: box_color[2],
+                       primitive_marker: :border }
+  end
+
+  def checkbox_value(id:)
+    $gtk.args.state.checkboxes[id][:checked]
+  end
+
+  def remove_checkbox(id:)
+    $gtk.args.state.checkboxes.delete(id)
+  end
+
+  def ui_update
+    if $gtk.args.inputs.mouse.click
+      $gtk.args.state.checkboxes.each do |_k, v|
+        v[:checked] = !v[:checked] if $gtk.args.inputs.mouse.inside_rect? v
+      end
     end
   end
 
